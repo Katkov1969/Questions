d479183cfc3d909ff16d2a0db552d1c8f91c100e
Katkov1969 <126957788+Katkov1969@users.noreply.github.com>
2024-08-20 17:37:26 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,45 @@
+
+# Задача "Цепочка вопросов":
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+api = "7242086577:AAFBXvpUeDZHu42sHyCYmzZ_wfgOiGdtXEE"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth =State()
+    weight =State()
+
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer('Введите свой рост')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer('Введите свой вес')
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    #calories = 10 * float(data['first']) + 6.25 * float(data['second']) - 5 * float(data['thierd']) + 5
+    calories = 10 * float(data['weight']) + 6.25 * float(data['growth']) - 5 * float(data['age']) + 5
+
+    await message.answer(f'Ваша норма калорий {calories}')
+    await state.finish()
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
